4ab00a495d94a66c235df221d6457fd9c1c7fee4
Tony <tony@lkpo.net>
2021-04-29 18:35:01 +0000

    Actually fixed.

--- themes/Ultraviolent.js
+++ themes/Ultraviolent.js
@@ -1,4 +1,4 @@
-"background-color": "#242728",
-"foreground-color": "#c1c1c1",
-"cursor-color":     "#c1c1c1",
-"color-palette-overrides": ["#242728","#ff0090","#b6ff00","#fff727","#47e0fb","#d731ff","#0effbb","#e1e1e1","#636667","#fb58b4","#deff8c","#ebe087","#7fecff","#e681ff","#69fcd3","#f9f9f5"]
+t.prefs_.set('background-color',"#242728",
+t.prefs_.set('foreground-color',"#c1c1c1",
+t.prefs_.set('cursor-color', "#c1c1c1",
+t.prefs_.set('color-palette-overrides',["#242728","#ff0090","#b6ff00","#fff727","#47e0fb","#d731ff","#0effbb","#e1e1e1","#636667","#fb58b4","#deff8c","#ebe087","#7fecff","#e681ff","#69fcd3","#f9f9f5"]
