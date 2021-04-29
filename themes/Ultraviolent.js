67758c712045766427e7e7b6beb743e64f6e0828
Tony <tony@lkpo.net>
2021-04-29 21:23:36 +0000

    actually really fixed perhaps

--- themes/Ultraviolent.js
+++ themes/Ultraviolent.js
@@ -1,4 +1,41 @@
-t.prefs_.set('background-color',"#242728",
-t.prefs_.set('foreground-color',"#c1c1c1",
-t.prefs_.set('cursor-color', "#c1c1c1",
-t.prefs_.set('color-palette-overrides',["#242728","#ff0090","#b6ff00","#fff727","#47e0fb","#d731ff","#0effbb","#e1e1e1","#636667","#fb58b4","#deff8c","#ebe087","#7fecff","#e681ff","#69fcd3","#f9f9f5"]
+BLACK = "#242728";
+RED = "#ff0090";
+GREEN = "#b6ff00";
+YELLOW = "#fff727";
+BLUE = "#47e0fb";
+MAGENTA = "#d731ff";
+CYAN = "#0effbb";
+WHITE = "#e1e1e1";
+BRIGHT_BLACK = "#636667";
+BRIGHT_RED = "#fb58b4";
+BRIGHT_GREEN = "#deff8c";
+BRIGHT_YELLOW = "#ebe087";
+BRIGHT_BLUE = "#7fecff";
+BRIGHT_MAGENTA = "#e681ff";
+BRIGHT_CYAN = "#69fcd3";
+BRIGHT_WHITE = "#f9f9f5";
+FOREGROUND = "#c1c1c1";
+BACKGROUND = "#242728";
+CURSOR = "rgba(193,193,193,1.0)";
+
+t.prefs_.set("color-palette-overrides", [
+  BLACK,
+  RED,
+  GREEN,
+  YELLOW,
+  BLUE,
+  MAGENTA,
+  CYAN,
+  WHITE,
+  BRIGHT_BLACK,
+  BRIGHT_RED,
+  BRIGHT_GREEN,
+  BRIGHT_YELLOW,
+  BRIGHT_BLUE,
+  BRIGHT_MAGENTA,
+  BRIGHT_CYAN,
+  BRIGHT_WHITE,
+]);
+t.prefs_.set("foreground-color", FOREGROUND);
+t.prefs_.set("background-color", BACKGROUND);
+t.prefs_.set("cursor-color", CURSOR);
